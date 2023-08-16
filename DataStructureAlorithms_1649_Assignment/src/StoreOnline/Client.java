151153160d55eec52bc69b5658305f2fde9d3975
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2023-08-16 18:29:26 +0700

    Import and Export Order

--- DataStructureAlorithms_1649_Assignment/src/StoreOnline/Client.java
+++ DataStructureAlorithms_1649_Assignment/src/StoreOnline/Client.java
@@ -24,6 +24,7 @@ public class Client {
     }
 
     public static void sendOrdertoServer(){
+        long totalTime = 0;
         try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -32,23 +33,26 @@ public class Client {
 
             System.out.println("Send order to server ");
 
-            while (!orderQueue.isEmpty()){
+            long startTime = System.currentTimeMillis();
+
+            while (!orderQueue.isEmpty()) {
                 out.println(orderQueue.poll());
                 System.out.print(".");
                 orderStack.push(in.readLine());
             }
-            System.out.println();
-            if (currentSize == orderStack.size()){
-                System.out.println("All orders delivered successfully");
-            }
+            long endTime = System.currentTimeMillis();
 
-//            for (String order : orderList) {
-//                out.println(order); // Gửi đơn hàng lên Server
-//                String response = in.readLine(); // Nhận phản hồi từ Server
-//                System.out.println("Server response: " + response);
-//            }
+            totalTime = endTime - startTime;
+            System.out.println();
+
+
+
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("All orders delivered successfully: " + totalTime + "ms");
         }
 
     }
@@ -67,27 +71,56 @@ public class Client {
             System.out.println(e.getMessage());
         }
         finally {
-            System.out.println("Read Order From File Successful!");
+            System.out.println("All orders read from file order_list.txt have been added to Queue!");
         }
+    }
+
+    public static void exportOrder(){
+        String filename = "D:\\GitHubClone\\1649_DataStructure-Alorithms_Assignment\\DataStructureAlorithms_1649_Assignment\\src\\data\\order_unbox.txt";
+
+        try {
+            FileWriter writer = new FileWriter(filename);
+
+            while (!orderStack.isEmpty()) {
+                String item = orderStack.pop();
+            }
+
+            writer.close();
+            System.out.println("All orders from stack have been exported to order_unbox.txt!");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public static void showOrderHasDelivered(){
+        System.out.println(orderStack);
+    }
+
+    public static void addOrdertoQueue(){
+        System.out.print("Enter your order: ");
+        orderQueue.offer(scn.nextLine());
+        System.out.println("Add to Queue successful");
     }
 
     public static void mainMenu(){
         int choice = 0;
-        while (choice != 5){
+        while (choice != 6){
             showMenu();
             System.out.print("Enter your choice: ");
             choice = Integer.parseInt(scn.nextLine());
-
+            System.out.println();
             switch (choice){
                 case 1:
                     readOrderListFromFile();
-
+                    break;
+                case 2:
+                    addOrdertoQueue();
                     break;
                 case 3:
                     sendOrdertoServer();
                     break;
                 case 4:
-
+                    showOrderHasDelivered();
                     break;
                 case 5:
 
@@ -100,13 +133,14 @@ public class Client {
     }
 
     public static void showMenu(){
+        System.out.println("_______________________________________");
         System.out.println("1. Read Order form File");
         System.out.println("2. Add Order to Queue");
         System.out.println("3. Send Order to Server");
-        System.out.println("4. Show orders have been delivered");
-        System.out.println("5. Unbox the latest delivered order");
+        System.out.println("4. Show delivered orders");
+        System.out.println("5. Unbox and Export all Orders");
         System.out.println("6. Quit");
-
+        System.out.println("_______________________________________");
     }
 
 }
