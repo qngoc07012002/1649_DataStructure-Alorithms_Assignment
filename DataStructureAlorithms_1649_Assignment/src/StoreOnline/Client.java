0e70d5621dd8f6caf40db5516c1563de91c540da
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2023-08-16 15:32:28 +0700

    Test Client

--- DataStructureAlorithms_1649_Assignment/src/StoreOnline/Client.java
+++ DataStructureAlorithms_1649_Assignment/src/StoreOnline/Client.java
@@ -3,26 +3,27 @@ package StoreOnline;
 import java.io.*;
 import java.net.*;
 
+
 public class Client {
-    private static final String SERVER_ADDRESS = "localhost";
-    private static final int SERVER_PORT = 2222;
+    private static final String SERVER_IP = "127.0.0.1"; // Địa chỉ IP của Server
+    private static final int SERVER_PORT = 1111; // Cổng của Server
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
-
-            System.out.println("Connected to server");
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
             String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("Server response: " + in.readLine());
+            while ((userInput = consoleReader.readLine()) != null) {
+                out.println(userInput); // Gửi đơn hàng lên Server
+                String response = in.readLine(); // Nhận phản hồi từ Server
+                System.out.println("Server response: " + response);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
 
--- DataStructureAlorithms_1649_Assignment/src/StoreOnline/Server.java
+++ DataStructureAlorithms_1649_Assignment/src/StoreOnline/Server.java
@@ -2,29 +2,30 @@ package StoreOnline;
 
 import java.io.*;
 import java.net.*;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Server {
-    private static final int PORT = 2222;
+    private static final int PORT = 1111;
     private static Queue<String> orderQueue = new ConcurrentLinkedQueue<>();
-    private static Stack<String> orderHistory = new Stack<>();
-    private static ExecutorService executorService = Executors.newFixedThreadPool(5);
+    private static Queue<PrintWriter> clientWriters = new ConcurrentLinkedQueue<>();
+    private static ExecutorService executorService = Executors.newFixedThreadPool(3);
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server is listening on port " + PORT);
 
+            executorService.submit(new ProcessOrders());
+
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                clientWriters.add(clientWriter);
+
+                Thread clientThread = new Thread(new ClientHandler(clientSocket, clientWriter));
                 clientThread.start();
-                mainMenu();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -33,29 +34,25 @@ public class Server {
 
     private static class ClientHandler implements Runnable {
         private Socket clientSocket;
-        private PrintWriter out;
-        private BufferedReader in;
+        private PrintWriter clientWriter;
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(Socket socket, PrintWriter writer) {
             this.clientSocket = socket;
+            this.clientWriter = writer;
         }
 
         @Override
         public void run() {
             try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
                     System.out.println("Received order: " + inputLine);
                     orderQueue.add(inputLine);
-
-                    out.println("Order received");
                 }
 
                 in.close();
-                out.close();
                 clientSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -63,77 +60,24 @@ public class Server {
         }
     }
 
-    private static class OrderProcessing implements Runnable {
-        private String order;
-
-        public OrderProcessing(String order) {
-            this.order = order;
-        }
-
+    private static class ProcessOrders implements Runnable {
         @Override
         public void run() {
-            orderHistory.push(order);
-            System.out.println("Processing order: " + order);
-
-            // Đợi một chút trước khi kiểm tra lại
-            try {
-                Thread.sleep(1000); // Đợi 1 giây
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    // Phần MainMenu và các phần liên quan
-    public static void showMenu() {
-        System.out.println("\nMenu:");
-        System.out.println("1. Process Orders");
-        System.out.println("2. Print Order History");
-        System.out.println("3. Disconnect");
-        System.out.print("Enter your choice: ");
-    }
-
-    public static void mainMenu() {
-        boolean exit = false;
-        while (!exit) {
-            showMenu();
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                int choice = Integer.parseInt(reader.readLine());
-
-                switch (choice) {
-                    case 1:
-                        processOrders();
-                        break;
-                    case 2:
-                        printOrderHistory();
-                        break;
-                    case 3:
-                        exit = true;
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please choose again.");
+            while (true) {
+                String order = orderQueue.poll();
+                if (order != null) {
+                    System.out.println("Processing order: " + order);
+                    sendProcessedOrder(order);
                 }
-            } catch (IOException | NumberFormatException e) {
-                e.printStackTrace();
             }
         }
     }
 
-    private static void processOrders() {
-        while (!orderQueue.isEmpty()) {
-            String order = orderQueue.poll();
-            executorService.submit(new OrderProcessing(order));
+    private static void sendProcessedOrder(String order) {
+        synchronized (clientWriters) {
+            for (PrintWriter writer : clientWriters) {
+                writer.println("Processed order: " + order);
+            }
         }
     }
-
-
-    private static void printOrderHistory() {
-        System.out.println("Order History:");
-        for (String order : orderHistory) {
-            System.out.println(order);
-        }
-    }
-
-    // Phần còn lại của mã bạn đã đưa ra
 }
